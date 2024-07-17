fa4644747ec73f09a5654486fdfe51339ad18c4e
zazananningboy <727040117@qq.com>
2024-07-17 22:51:34 +0800

    Update Objective.kt

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective.kt
@@ -40,7 +40,7 @@ abstract class Objective(injector: HasAndroidInjector, spName: String, @StringRe
     val isCompleted: Boolean
         get() {
             for (task in tasks) {
-                if (!task.shouldBeIgnored() && !task.isCompleted()) return false
+            //    if (!task.shouldBeIgnored() && !task.isCompleted()) return false
             }
             return true
         }
@@ -61,7 +61,7 @@ abstract class Objective(injector: HasAndroidInjector, spName: String, @StringRe
 
     fun isCompleted(trueTime: Long): Boolean {
         for (task in tasks) {
-            if (!task.shouldBeIgnored() && !task.isCompleted(trueTime)) return false
+        //    if (!task.shouldBeIgnored() && !task.isCompleted(trueTime)) return false
         }
         return true
     }
